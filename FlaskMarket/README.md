e5f20d0eb9156297012ae2f19eb8277f8a97fc7c
Hussain <hussain.mohammad@valueleaf.com>
2021-03-23 10:59:45 +0530

    p5: Models and Databases

--- FlaskMarket/README.md
+++ FlaskMarket/README.md
@@ -1,18 +1,36 @@
 ## Project on Flask
 
 ## Install Flask
+```
 $ mkdir FlaskMarket
 $ cd FlaskMarket
 $ pipenv shell
 $ pip install flask
+```
 
 ## setting flask
+```
 $ export FLASK_APP=market.py
-
-## run flask server
-$ flask run
+```
 
 ## Setting Debug mode on - helps to restart server on any changes
+```
 $ export FLASK_DEBUG=1
+```
 
+## run flask server
+```
+$ flask run
+```
 
+## Install SQLAlchemy
+```
+$ pip install flask-sqlalchemy
+```
+
+## Create Sqlite DB
+```
+$ python
+>>> from app import db
+>>> db.create_all()
+```
--- FlaskMarket/market.py
+++ FlaskMarket/market.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
 
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
 # @app.route('/')
 # def hello_world():
     # return '<h1>Hello, World!</h1>'
@@ -20,9 +32,10 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        { 'id': 1, 'name': 'Phone', 'barcode': '8933224455697', 'price': 500},
-        { 'id': 2, 'name': 'Laptop', 'barcode': '4554323322756', 'price': 900},
-        { 'id': 3, 'name': 'Keboard', 'barcode': '3242564455601', 'price': 150}
-    ]
+    # items = [
+    #     { 'id': 1, 'name': 'Phone', 'barcode': '8933224455697', 'price': 500},
+    #     { 'id': 2, 'name': 'Laptop', 'barcode': '4554323322756', 'price': 900},
+    #     { 'id': 3, 'name': 'Keboard', 'barcode': '3242564455601', 'price': 150}
+    # ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
