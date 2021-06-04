2fa6f737981e6272b7973972d9a8112c17609443
Mohammad Hussain <mohammad.hussain@nutanix.com>
2021-06-04 12:23:17 +0530

    p-15 Item purchasing part-2

--- FlaskMarket/market/models.py
+++ FlaskMarket/market/models.py
@@ -2,9 +2,11 @@ from market import db, login_manager
 from market import bcrypt
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,6 +34,11 @@ class User(db.Model, UserMixin):
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
+    def can_purchase(self, item_obj):
+        print('item', item_obj)
+        return self.budget >= item_obj.price
+
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -42,3 +49,8 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.name}'
+
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
--- FlaskMarket/market/routes.py
+++ FlaskMarket/market/routes.py
@@ -1,15 +1,15 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from market import app
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm
 from market import db
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 # @app.route('/')
 # def hello_world():
-    # return '<h1>Hello, World!</h1>'
+#     return '<h1>Hello, World!</h1>'
 
-## Dynamic Route
+# Dynamic Route
 # @app.route('/about/<username>')
 # def about_page(username):
 #     return f'<h1>This is the about page of {username}</h1>'
@@ -20,16 +20,34 @@ from flask_login import login_user, logout_user, login_required
 def home_page():
     return render_template('home.html')
 
-@app.route('/market')
+
+@app.route('/market', methods=['GET', 'POST'])
 @login_required
 def market_page():
     # items = [
     #     { 'id': 1, 'name': 'Phone', 'barcode': '8933224455697', 'price': 500},
     #     { 'id': 2, 'name': 'Laptop', 'barcode': '4554323322756', 'price': 900},
-    #     { 'id': 3, 'name': 'Keboard', 'barcode': '3242564455601', 'price': 150}
+    #     { 'id': 3, 'name': 'Keyboard', 'barcode': '3242564455601', 'price': 150}
     # ]
-    items = Item.query.all()
-    return render_template('market.html', items=items)
+    purchase_form = PurchaseItemForm()
+    if request.method == 'POST':
+        purchased_item = request.form.get('purchased_item')
+        p_item_obj = Item.query.filter_by(name=purchased_item).first()
+        if p_item_obj:
+            if current_user.can_purchase(p_item_obj):
+                p_item_obj.buy(current_user)
+                flash(f"Congratulations! You purchased {p_item_obj.name} for {p_item_obj.price}$.", category='success')
+            else:
+                flash(f"Unfortunately, you don't have enough money to purchase {p_item_obj.name}", category='danger')
+        return redirect(url_for('market_page'))
+
+    if request.method == 'GET':
+        # List all the items
+        # items = Item.query.all()
+        items = Item.query.filter_by(owner=None)
+        print(items)
+        return render_template('market.html', items=items, purchase_form=purchase_form)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
@@ -46,11 +64,13 @@ def register_page():
         flash(f'Account created successfully! You are now logged in as {user_to_create.username}', category='success')
 
         return redirect(url_for('market_page'))
-    if form.errors != {}: #if there are no errors from the validations
+    # if there are no errors from the validations
+    if form.errors != {}:
         for err_msg in form.errors.values():
             flash(f'There was an error while creating a user: {err_msg}', category='danger')
 
     return render_template('register.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
@@ -59,12 +79,13 @@ def login_page():
         attempted_user = User.query.filter_by(username=form.username.data).first()
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
             login_user(attempted_user)
-            flash(f'Sucess! you are logged in as: {attempted_user.username}', category='success')
+            flash(f'Success! you are logged in as: {attempted_user.username}', category='success')
             return redirect(url_for('market_page'))
         else:
             flash(f'Username and password are not match! Please try again', category='danger')
 
     return render_template('login.html', form=form)
+
 
 @app.route('/logout')
 def logout_page():
